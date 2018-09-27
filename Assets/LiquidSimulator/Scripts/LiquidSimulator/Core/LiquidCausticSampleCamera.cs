538664d04d936bd8b05137dca54fbeca3578eee4
lusen <huancr@outlook.com>
2018-09-27 20:29:02 +0800

    焦散渲染相机修改

--- Assets/LiquidSimulator/Scripts/LiquidSimulator/Core/LiquidCausticSampleCamera.cs
+++ Assets/LiquidSimulator/Scripts/LiquidSimulator/Core/LiquidCausticSampleCamera.cs
@@ -1,18 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LiquidCausticSampleCamera : MonoBehaviour
+namespace ASL.LiquidSimulator
 {
-
-    // Use this for initialization
-    void Start()
+    public class LiquidCausticSampleCamera : MonoBehaviour
     {
 
-    }
+        // Use this for initialization
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
+        }
 
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
